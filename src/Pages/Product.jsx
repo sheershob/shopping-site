bbe04f4e141f2ae94ead4bf0ddf0c0aeb6a4ceb3
Sheersho Banerjee <sheersho.banerjee@gmail.com>
2025-05-01 19:07:48 +0530

    second commit

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find(item => item.id === parseInt(productId));
   return (
     <div>
       
